0009056173b7994eb22e7902bb4b6e41d3a76a3b
Kyle VanderBeek <kylev@kylev.com>
2019-12-25 22:36:18 -0800

    Start prep for real lambda deployment.

---
+++ tf/opsbot.tf
@@ -0,0 +1,41 @@
+variable "function_name" {
+  default = "opsbot_lambda"
+}
+
+variable "handler" {
+  default = "opsbot.handler.sns_handler"
+}
+
+variable "runtime" {
+  default = "python3.8"
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = var.handler
+  runtime          = var.runtime
+  function_name    = var.function_name
+  s3_bucket        = "kylev-utility"
+  s3_key           = "opsbot/lambda.zip"
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name        = "${var.function_name}_exec"
+  path        = "/"
+  description = "Allows Lambda Function to call AWS services on your behalf."
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
