de95bcde1798010a0997bb1be79378ac92e2e1fa
Kyle VanderBeek <kylev@kylev.com>
2019-12-25 21:27:44 -0800

    Switch toward striaght pip and setuptools.

---
+++ opsbot/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="opsbot",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=['boto3>=1.10', 'nose'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
