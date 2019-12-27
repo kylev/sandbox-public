a8c58d762683e2b6c327f2cdbf86e0ce582807df
Kyle VanderBeek <kylev@kylev.com>
2019-12-26 17:38:36 -0800

    Better model for the package installation.
    Real requirements go in requirements.txt. pip install is only used to
    add optional stuff in the development env.

--- opsbot/setup.py
+++ opsbot/setup.py
@@ -4,7 +4,8 @@ setup(
     name="opsbot",
     version="0.1",
     packages=find_packages(),
-    install_requires=['boto3>=1.10', 'nose'],
     test_suite='nose.collector',
-    tests_require=['moto', 'nose'],
+    extras_require={
+        'development': ['moto', 'nose'],
+    }
 )
