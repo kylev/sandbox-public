e44f741b4edbb3b6ac95a31644f954210d964b2f
Kyle VanderBeek <kylev@kylev.com>
2019-12-26 16:41:34 -0800

    Connect to API Gateway so Opsbot can be invoked by web handler.

--- tf/opsbot.tf
+++ tf/opsbot.tf
@@ -10,6 +10,7 @@ variable "runtime" {
   default = "python3.8"
 }
 
+# Function
 resource "aws_lambda_function" "opsbot_function" {
   role             = aws_iam_role.opsbot_role.arn
   handler          = var.handler
@@ -23,6 +24,53 @@ resource "aws_lambda_function" "opsbot_function" {
   }
 }
 
+# API Gateway
+resource "aws_api_gateway_rest_api" "opsbot_api" {
+  name        = "OpsbotApi"
+  description = "Opsbot HTTP API."
+}
+
+resource "aws_api_gateway_resource" "opsbot_gw_resource" {
+  rest_api_id = aws_api_gateway_rest_api.opsbot_api.id
+  parent_id   = aws_api_gateway_rest_api.opsbot_api.root_resource_id
+  path_part   = "opsbot"
+}
+
+resource "aws_api_gateway_method" "opsbot_method" {
+  rest_api_id   = aws_api_gateway_rest_api.opsbot_api.id
+  resource_id   = aws_api_gateway_resource.opsbot_gw_resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "opsbot_integration" {
+  rest_api_id = aws_api_gateway_rest_api.opsbot_api.id
+  resource_id = aws_api_gateway_method.opsbot_method.resource_id
+  http_method = aws_api_gateway_method.opsbot_method.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.opsbot_function.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "opsbot_deployment_test" {
+  depends_on = [
+    aws_api_gateway_integration.opsbot_integration,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.opsbot_api.id
+  stage_name  = "testing"
+}
+
+resource "aws_lambda_permission" "opsbot_lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.opsbot_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn   = "${aws_api_gateway_rest_api.opsbot_api.execution_arn}/*/*"
+}
+
+# Permissions
 data "aws_iam_policy_document" "opsbot_assume_policy_doc" {
   statement {
     actions = ["sts:AssumeRole"]
